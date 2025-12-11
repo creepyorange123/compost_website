0e4e3d4f3b17f23e87f93874c7698932ed263e15
Hanning Lu <gracelu323@gmail.com>
2025-12-10 20:59:42 -0500

    waste calculator formatted, started calculator

--- compost-website/app/for-home/components/WasteCalculator.tsx
+++ compost-website/app/for-home/components/WasteCalculator.tsx
@@ -1,6 +1,7 @@
-import { ReactCompareSlider } from "react-compare-slider";
-import { useEffect, useState } from "react";
-import styles from '../../../styles/Text.module.css';
+import { ReactCompareSlider, ReactCompareSliderHandle } from "react-compare-slider";
+import { useState } from "react";
+import image_styles from '../styles/Calculator.module.css';
+import text_styles from '../../../styles/Text.module.css';
 import Calculator from "./Calculator";
 import useImageChecker from "@/hooks/useImageChecker";
 
@@ -11,21 +12,31 @@ export function WasteCalculator() {
     const errorMsg2 = useImageChecker({ src: '/full-trash.png' });
 
     return (
-        <div className = {styles.bodyContainer}>
+        <div className = {text_styles.bodyContainer}>
+            <div className = {text_styles.title}>Waste Reduction Calculator</div>
+
             {errorMsg1 || errorMsg2 ?
                 <p>Error loading comparison slider.</p>
                 :
-                <ReactCompareSlider
-                    itemOne={<img
-                        src='/full-trash.png'
-                        alt={"full trash"} />}
-                    itemTwo={<img
-                        src='/full-trash.png'
-                        alt={"compost"} />}
-                    position={position}
-                    onPositionChange={setPosition}
-                />
-            }
+                <div className = {image_styles.sliderContainer}>
+                    <ReactCompareSlider
+                        itemOne={<img
+                            src='/full-trash.png'
+                            alt={"full trash"}
+                        />}
+                        itemTwo={<img
+                            src='/full-trash.png'
+                            alt={"compost"}
+                        />}
+                        position={position}
+                        onPositionChange={setPosition}
+                    />
+                </div>
+            }   
+            <div className = {text_styles.body}>
+                Toggle the slider above to decide how much of the waste above will be composed!<br />
+                Currently: {Math.round(position)}%
+            </div>
             <Calculator position={position} />
         </div>
     );
